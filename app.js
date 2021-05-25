057cdb034ede7dd9a7c3064d46af91f58c34738f
adribasn <adrien.basyn@outlook.com>
2021-05-25 12:04:09 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,106 @@
+"use strict"
+const keyOne = document.getElementById('key-one');
+const keyTwo = document.getElementById('key-two');
+const keyThree = document.getElementById('key-three');
+
+const keyFour = document.getElementById('key-four');
+const keyFive = document.getElementById('key-five');
+const keySix = document.getElementById('key-six');
+
+const keySeven = document.getElementById('key-seven');
+const keyEight = document.getElementById('key-eight');
+const keyNine = document.getElementById('key-nine');
+
+window.addEventListener('keydown', (event) => {
+    if (event.key == 'a') {
+        keyOne.style.backgroundColor = "#932432";
+        keyOne.style.color = "#fff";
+        let sfx = new Audio();
+        sfx.src = 'sounds/808 - Boomin.wav'
+        sfx.play();
+        window.setTimeout(() => {
+            keyOne.style.backgroundColor = "#fff";
+            keyOne.style.color = "#000";
+        }, 200);
+    } else if (event.key == 'z') {
+        keyTwo.style.backgroundColor = "#932432";
+        keyTwo.style.color = "#fff";
+        let sfx = new Audio();
+        sfx.src = 'sounds/Clap - Sauce.wav';
+        sfx.play();
+        window.setTimeout(() => {
+            keyTwo.style.backgroundColor = "#fff";
+            keyTwo.style.color = "#000";
+        }, 200);
+    } else if (event.key == 'e') {
+        keyThree.style.backgroundColor = "#932432";
+        keyThree.style.color = "#fff";
+        let sfx = new Audio();
+        sfx.src = 'sounds/clap (2).wav'
+        sfx.play();
+        window.setTimeout(() => {
+            keyThree.style.backgroundColor = "#fff";
+            keyThree.style.color = "#000";
+        }, 200);
+    } else if (event.key == 'q') {
+        keyFour.style.backgroundColor = "#932432";
+        keyFour.style.color = "#fff";
+        let sfx = new Audio();
+        sfx.src = 'sounds/Hi Hat - Hennesy.wav'
+        sfx.play();
+        window.setTimeout(() => {
+            keyFour.style.backgroundColor = "#fff";
+            keyFour.style.color = "#000";
+        }, 200);
+    } else if (event.key == 's') {
+        keyFive.style.backgroundColor = "#932432";
+        keyFive.style.color = "#fff";
+        let sfx = new Audio();
+        sfx.src = 'sounds/Kick - Marnell.wav'
+        sfx.play();
+        window.setTimeout(() => {
+            keyFive.style.backgroundColor = "#fff";
+            keyFive.style.color = "#000";
+        }, 200);
+    } else if (event.key == 'd') {
+        keySix.style.backgroundColor = "#932432";
+        keySix.style.color = "#fff";
+        let sfx = new Audio();
+        sfx.src = 'sounds/Open Hat - Cali.wav'
+        sfx.play();
+        window.setTimeout(() => {
+            keySix.style.backgroundColor = "#fff";
+            keySix.style.color = "#000";
+        }, 200);
+    } else if (event.key == 'w') {
+        keySeven.style.backgroundColor = "#932432";
+        keySeven.style.color = "#fff";
+        let sfx = new Audio();
+        sfx.src = 'sounds/open hat 1.wav';
+        sfx.play();
+        window.setTimeout(() => {
+            keySeven.style.backgroundColor = "#fff";
+            keySeven.style.color = "#000";
+        }, 200);
+    } else if (event.key == 'x') {
+        keyEight.style.backgroundColor = "#932432";
+        keyEight.style.color = "#fff";
+        let sfx = new Audio();
+        sfx.src = 'sounds/perc (3).wav';
+        sfx.play();
+        window.setTimeout(() => {
+            keyEight.style.backgroundColor = "#fff";
+            keyEight.style.color = "#000";
+        }, 200);
+    } else if (event.key == 'c') {
+        keyNine.style.backgroundColor = "#932432";
+        keyNine.style.color = "#fff";
+        let sfx = new Audio();
+        sfx.src = 'sounds/snare (4).wav';
+        sfx.play();
+        window.setTimeout(() => {
+            keyNine.style.backgroundColor = "#fff";
+            keyNine.style.color = "#000";
+        }, 200);
+    }
+})
